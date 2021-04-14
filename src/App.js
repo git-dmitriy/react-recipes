76dd1dec745fec0b73e06546388e9e34fd7635e4
Dmitriy <shalberkind@mail.ru>
2021-04-15 00:14:35 +0300

    Use parameters for category page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contacts } from './pages/Contacts';
 import { NotFound } from './pages/NotFound';
+import { Category } from './pages/Category';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/contacts' component={Contacts} />
+            <Route path='/category/:name' component={Category} />
             <Route component={NotFound} />
           </Switch>
         </main>
--- src/components/Category/CategoryItem.jsx
+++ src/components/Category/CategoryItem.jsx
@@ -16,7 +16,7 @@ export default function CategoryItem({
         <p>{strCategoryDescription.slice(0, 60)}...</p>
       </div>
       <div className='card-action'>
-        <Link to={`/category/${idCategory}`} className='btn'>
+        <Link to={`/category/${strCategory}`} className='btn'>
           Watch category
         </Link>
       </div>
