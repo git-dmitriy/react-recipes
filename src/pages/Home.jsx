d8e12c05d277dbce54f00cdce09c81969279da4b
Dmitriy <shalberkind@mail.ru>
2021-04-14 23:12:00 +0300

    Create main page with categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { useState, useEffect } from 'react';
+import { getAllCategories } from '../api';
+import { Preloader } from '../components/Preloader/Preloader';
+import CategoryList from '../components/Category/CategoryList';
+
 export function Home() {
-  return <h1>Home page</h1>;
+  const [catalog, setCatalog] = useState([]);
+
+  useEffect(() => {
+    getAllCategories().then((data) => {
+      setCatalog(data.categories);
+    });
+  }, []);
+
+  return (
+    <>{!catalog.length ? <Preloader /> : <CategoryList catalog={catalog} />}</>
+  );
 }
