a681558be6494cecb473c1068ec1264ad4fca0e5
Dmitriy <shalberkind@mail.ru>
2021-04-15 23:54:53 +0300

    Change link text to Recipes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,7 +9,7 @@ export function Header() {
         </Link>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/'>Recipes</Link>
           </li>
           <li>
             <Link to='/about'>About</Link>
