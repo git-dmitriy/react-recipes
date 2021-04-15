540fff3041300b2c8d079c62e921a55e08976758
Dmitriy <shalberkind@mail.ru>
2021-04-15 23:33:51 +0300

    Add preloader

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMealById } from '../api';
+import { Preloader } from '../components/Preloader/Preloader';
 import { Ingredients } from '../components/Recipe/Ingredients';
 import { YoutubeIframe } from '../components/Youtube/YoutubeIframe';
 
@@ -22,34 +23,40 @@ export function Recipe() {
 
   return (
     <>
-      <div className='row'>
-        <div className='col xl6 l6 m12'>
-          {recipe.strMealThumb ? (
-            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-          ) : (
-            <img src={placeholder + imgPlaceholder} alt={recipe.strMeal} />
-          )}
-        </div>
+      {!Object.keys(recipe).length ? (
+        <Preloader />
+      ) : (
+        <>
+          <div className='row'>
+            <div className='col xl6 l6 m12'>
+              {recipe.strMealThumb ? (
+                <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+              ) : (
+                <img src={placeholder + imgPlaceholder} alt={recipe.strMeal} />
+              )}
+            </div>
 
-        <div className='col xl6 l6 m12'>
-          <h2>{recipe.strMeal}</h2>
-          <h3>Category: {recipe.strCategory || 'unknown'}</h3>
-          <h3>Country of origin: {recipe.strArea || 'unknown'}</h3>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col xl12'>
-          <p className='flow-text'>{recipe.strInstructions}</p>
-        </div>
-        <div className='col xl12 m12 s12'>
-          <Ingredients props={recipe} />
-        </div>
-        {youtubeLink.length ? (
-          <div className='col xl12 m12 s12'>
-            <YoutubeIframe address={youtubeLink.slice(32)} />
+            <div className='col xl6 l6 m12'>
+              <h2>{recipe.strMeal}</h2>
+              <h3>Category: {recipe.strCategory || 'unknown'}</h3>
+              <h3>Country of origin: {recipe.strArea || 'unknown'}</h3>
+            </div>
           </div>
-        ) : null}
-      </div>
+          <div className='row'>
+            <div className='col xl12'>
+              <p className='flow-text'>{recipe.strInstructions}</p>
+            </div>
+            <div className='col xl12 m12 s12'>
+              <Ingredients props={recipe} />
+            </div>
+            {youtubeLink.length ? (
+              <div className='col xl12 m12 s12'>
+                <YoutubeIframe address={youtubeLink.slice(32)} />
+              </div>
+            ) : null}
+          </div>
+        </>
+      )}
     </>
   );
 }
