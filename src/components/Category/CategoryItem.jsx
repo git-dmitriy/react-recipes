3bb191efe1d1d4a5e57c6c67a604ba1e2b931f4c
git-dmitriy <shalberkind@mail.ru>
2021-08-16 23:43:54 +0300

    Styling the category components

--- src/components/Category/CategoryItem.jsx
+++ src/components/Category/CategoryItem.jsx
@@ -7,19 +7,24 @@ export default function CategoryItem({
   strCategoryDescription,
 }) {
   return (
-    <div className='card'>
-      <div className='card-image'>
-        <img src={strCategoryThumb} alt={strCategory} />
+    <li className='flex bg-gray-200 rounded-lg p-4 m-2'>
+      <div className='w-64 bg-gray-400 rounded-lg flex items-center justify-center'>
+        <img
+          className='w-full'
+          src={strCategoryThumb}
+          alt={strCategory}
+        />
       </div>
-      <div className='card-content'>
-        <span className='card-title'>{strCategory}</span>
-        <p>{strCategoryDescription.slice(0, 60)}...</p>
-      </div>
-      <div className='card-action'>
-        <Link to={`/category/${strCategory}`} className='btn'>
+      <div className='flex flex-col items-start ml-4'>
+        <h4 className='text-xl font-semibold'>{strCategory}</h4>
+        <p className='text-sm'>{strCategoryDescription.slice(0, 60)}...</p>
+        <Link
+          to={`/category/${strCategory}`}
+          className='p-2 leading-none rounded font-medium mt-3 bg-gray-400 text-xs uppercase'
+        >
           Watch category
         </Link>
       </div>
-    </div>
+    </li>
   );
 }
--- src/components/Category/CategoryList.jsx
+++ src/components/Category/CategoryList.jsx
@@ -1,11 +1,12 @@
 import CategoryItem from './CategoryItem';
 
 export default function CategoryList({ catalog = [] }) {
+  console.log(catalog);
   return (
-    <div className='list'>
+    <ul className='mx-auto grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 xl:max-w-6xl max-w-4xl'>
       {catalog.map((el) => (
         <CategoryItem key={el.idCategory} {...el} />
       ))}
-    </div>
+    </ul>
   );
 }
