c2f902dbfa6c6fbe2adf128a638e65ffa43a9920
git-dmitriy <shalberkind@mail.ru>
2021-08-27 00:00:10 +0300

    Add a category description card

---
+++ src/components/Category/AboutCategory.jsx
@@ -0,0 +1,24 @@
+const AboutCategory = ({ categoryInfo }) => {
+  console.log(
+    '🚀 ~ file: AboutCategory.jsx ~ line 4 ~ categoryInfo',
+    categoryInfo
+  );
+  const { strCategoryThumb, strCategory, strCategoryDescription } =
+    categoryInfo;
+
+  return (
+    <div className='hidden sm:block mx-auto px-4 xl:max-w-6xl max-w-4xl'>
+      <div className='flex bg-yellow-100 rounded-lg p-4 m-2'>
+        <div className='sm:w-2/4 w-2/5 bg-yellow-50 rounded-lg flex items-center justify-center'>
+          <img className='w-full' src={strCategoryThumb} alt={strCategory} />
+        </div>
+        <div className='sm:w-2/4 w-3/5 flex flex-col items-start ml-4 border-l-2 border-yellow-300 pl-4'>
+          <h4 className='text-xl font-semibold'>{strCategory}</h4>
+          <p className='text-md'>{strCategoryDescription}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutCategory;
