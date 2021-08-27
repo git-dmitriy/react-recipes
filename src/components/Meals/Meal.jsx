ab646a3a8cc614ef9a6fb90b6c34af6236eb9677
git-dmitriy <shalberkind@mail.ru>
2021-08-27 17:07:40 +0300

    Fix meal card styles

--- src/components/Meals/Meal.jsx
+++ src/components/Meals/Meal.jsx
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
+import { useContext } from 'react';
 import { FavoriteContext } from '../../context/favoritesContext';
 
 export function Meal(props) {
-  const [isFavorite, setIsFavorite] = useState();
   const { idMeal, strMeal, strMealThumb } = props;
   const { addToFavorites, removeFromFavorites, state } =
     useContext(FavoriteContext);
 
   return (
     <>
-      <li className='flex flex-col bg-yellow-200 rounded-lg p-4 m-2'>
-        <div className=' bg-green-100 rounded-lg'>
+      <li className='flex flex-col justify-between bg-yellow-200 rounded-lg p-4 m-2'>
+        <div className='bg-green-100 rounded-lg overflow-hidden'>
           <img className='w-full' src={strMealThumb} alt={strMeal} />
         </div>
         <div className='flex flex-col items-start mt-4'>
@@ -51,7 +50,7 @@ export function Meal(props) {
           </div>
           <Link
             to={`/meal/${idMeal}`}
-            className='p-2 leading-none rounded font-medium mt-3 bg-yellow-300 hover:bg-yellow-400 transition duration-200 text-xs uppercase'
+            className='w-full text-center py-5 leading-none rounded font-medium mt-3 bg-yellow-400 hover:bg-yellow-300 transition duration-200 text-md uppercase'
           >
             Watch the recipe
           </Link>
