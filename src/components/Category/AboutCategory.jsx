fcb36c651f67ab2a208f1c19cd395fbf3bd19d9c
git-dmitriy <shalberkind@mail.ru>
2021-08-27 16:28:48 +0300

    Add padding spacing

--- src/components/Category/AboutCategory.jsx
+++ src/components/Category/AboutCategory.jsx
@@ -7,12 +7,12 @@ const AboutCategory = ({ categoryInfo }) => {
     categoryInfo;
 
   return (
-    <div className='hidden sm:block mx-auto px-4 xl:max-w-6xl max-w-4xl'>
-      <div className='flex bg-yellow-100 rounded-lg p-4 m-2'>
+    <div className='hidden sm:block mx-auto xl:max-w-6xl max-w-4xl'>
+      <div className='flex bg-yellow-100 rounded-lg p-8 m-2'>
         <div className='sm:w-2/4 w-2/5 bg-yellow-50 rounded-lg flex items-center justify-center'>
           <img className='w-full' src={strCategoryThumb} alt={strCategory} />
         </div>
-        <div className='sm:w-2/4 w-3/5 flex flex-col items-start ml-4 border-l-2 border-yellow-300 pl-4'>
+        <div className='sm:w-2/4 w-3/5 flex flex-col items-start ml-8 pl-8 border-l-2 border-yellow-300'>
           <h4 className='text-xl font-semibold'>{strCategory}</h4>
           <p className='text-md'>{strCategoryDescription}</p>
         </div>
