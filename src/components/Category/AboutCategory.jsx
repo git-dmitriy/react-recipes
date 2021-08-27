fe6f2be3de59ed49e4ea27df1f545959ef036a43
git-dmitriy <shalberkind@mail.ru>
2021-08-27 18:45:22 +0300

    Add responsiveness for category card description

--- src/components/Category/AboutCategory.jsx
+++ src/components/Category/AboutCategory.jsx
@@ -1,8 +1,4 @@
 const AboutCategory = ({ categoryInfo }) => {
-  console.log(
-    '🚀 ~ file: AboutCategory.jsx ~ line 4 ~ categoryInfo',
-    categoryInfo
-  );
   const { strCategoryThumb, strCategory, strCategoryDescription } =
     categoryInfo;
 
@@ -12,7 +8,7 @@ const AboutCategory = ({ categoryInfo }) => {
         <div className='sm:w-2/4 w-2/5 bg-yellow-50 rounded-lg flex items-center justify-center'>
           <img className='w-full' src={strCategoryThumb} alt={strCategory} />
         </div>
-        <div className='sm:w-2/4 w-3/5 flex flex-col items-start ml-8 pl-8 border-l-2 border-yellow-300'>
+        <div className='sm:w-2/4 w-3/5 flex flex-col justify-around items-start ml-8 pl-8 border-l-2 border-yellow-300'>
           <h4 className='text-xl font-semibold'>{strCategory}</h4>
           <p className='text-md'>{strCategoryDescription}</p>
         </div>
