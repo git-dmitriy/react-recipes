4a6411c859761a4b96a61c7e93278b833f4e2079
git-dmitriy <shalberkind@mail.ru>
2021-08-28 10:24:52 +0300

    Add additional youtube iframe attributes

--- src/components/Youtube/YoutubeIframe.jsx
+++ src/components/Youtube/YoutubeIframe.jsx
@@ -7,6 +7,8 @@ export function YoutubeIframe({ address }) {
         width='100%'
         height='500px'
         src={`https://www.youtube.com/embed/${address}`}
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
       ></iframe>
     </div>
   );
