3e15bab0ec5bf99ef8bfbbee4b93c8c0eab644ab
git-dmitriy <shalberkind@mail.ru>
2021-11-23 23:59:39 +0300

    Fix layout for loader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,7 @@ export function Preloader() {
   let circle = 'h-5 w-5 bg-yellow-400 rounded-full';
 
   return (
-    <div className='flex justify-center my-8'>
+    <div className='fixed inset-0 h-screen flex justify-center items-center my-8'>
       <div className='flex'>
         <div className={`${circle} mr-1 animate-bounce`}></div>
         <div className={`${circle} mr-1 animate-bounce200`}></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         bounce200: 'bounce 1s infinite 200ms',
         bounce400: 'bounce 1s infinite 400ms',
       },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-100%)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+          },
+        },
+      },
       minHeight: {
         300: '300px',
       },
