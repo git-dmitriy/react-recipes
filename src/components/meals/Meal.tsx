aacef6a5cbd8913ba858c6f717f5fed61ab47188
git-dmitriy <shalberkind@mail.ru>
2022-01-25 21:58:23 +0300

    Refactoring the favorite toggle component

--- src/components/meals/Meal.tsx
+++ src/components/meals/Meal.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
-import { AppContext } from 'context/AppContext';
-import { AddToFavorite } from 'components/AddToFavorites';
+import { FavoriteToggle } from 'components/FavoriteToggle';
 import LazyLoad from 'react-lazyload';
 import { MealItemTypes } from 'appTypes';
 
@@ -9,18 +7,12 @@ type P = MealItemTypes;
 
 export const Meal: React.FC<P> = (props) => {
   const { idMeal, strMeal, strMealThumb } = props;
-  const { addToFavorites, removeFromFavorites, state } = useContext(AppContext);
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsFavorite(!!state.favorites.find((item) => item.idMeal === idMeal));
-  }, [state, idMeal]);
 
   return (
     <li className='relative w-full z-0 bg-white dark:bg-gray-100 rounded-3xl overflow-hidden border border-gray-200 dark:border-gray-900 dark:text-gray-900'>
       <Link
         to={`/meal/${idMeal}`}
-        className='block rounded-lg overflow-hidden min-h-300'
+        className='block rounded-lg overflow-hidden min-h-300 hover:opacity-90 transition-opacity'
       >
         <LazyLoad height={300} offset={100} once>
           <img className='w-full' src={strMealThumb} alt={strMeal} />
@@ -33,12 +25,7 @@ export const Meal: React.FC<P> = (props) => {
       </Link>
 
       <div className='absolute top-4 right-4 p-1 bg-white bg-opacity-50 backdrop-opacity-50 backdrop-filter backdrop-blur-2xl rounded-2xl'>
-        <AddToFavorite
-          isFavorite={isFavorite}
-          isDark={true}
-          addToFavorites={() => addToFavorites(props)}
-          removeFromFavorites={() => removeFromFavorites(props)}
-        />
+        <FavoriteToggle isDark={true} meal={props} />
       </div>
     </li>
   );
--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -6,7 +6,7 @@ import { Loader } from 'components/Loader';
 import { Ingredients } from 'components/recipe/Ingredients';
 import { YoutubeIframe } from 'components/YoutubeIframe';
 import { RecipeImage } from 'components/recipe/RecipeImage';
-import { AddToFavorite } from 'components/AddToFavorites';
+import { FavoriteToggle } from 'components/FavoriteToggle';
 import { CategoriesLink } from 'components/recipe/CategoriesLink';
 import { Layout } from 'components/layout/Layout';
 import { MealItemTypes } from 'appTypes';
@@ -14,9 +14,8 @@ import { MealItemTypes } from 'appTypes';
 export const Recipe: React.FC = () => {
   const { idMeal } = useParams();
   const [recipe, setRecipe] = useState<MealItemTypes | null>(null);
-  const [isFavorite, setIsFavorite] = useState(false);
 
-  const { state, addToFavorites, removeFromFavorites } = useContext(AppContext);
+  const { state } = useContext(AppContext);
 
   const [imgPlaceholder, setImgPlaceholder] = useState('');
   const [youtubeLink, setYoutubeLink] = useState('');
@@ -29,64 +28,54 @@ export const Recipe: React.FC = () => {
         setImgPlaceholder(placeholder + data.meals[0].strMeal);
         setYoutubeLink(data.meals[0].strYoutube);
       });
-
-      setIsFavorite(!!state.favorites.find((item) => item.idMeal === idMeal));
     }
-  }, [idMeal]);
+  }, [idMeal, state.favorites]);
 
   if (!recipe) {
-    return <div>no recipe...</div>;
+    return <Loader />;
   }
 
   return (
     <>
-      {!Object.keys(recipe).length ? (
-        <Loader />
-      ) : (
-        <Layout>
-          <div className='flex flex-col items-center mb-10'>
-            <div className='flex justify-center mb-5'>
-              <h2 className='font-semibold text-center text-2xl sm:text-3xl'>
-                {recipe.strMeal}
-              </h2>
+      <Layout>
+        <div className='flex flex-col items-center mb-10'>
+          <div className='flex justify-center mb-5'>
+            <h2 className='font-semibold text-center text-2xl sm:text-3xl'>
+              {recipe.strMeal}
+            </h2>
 
-              <AddToFavorite
-                addToFavorites={() => addToFavorites(recipe)}
-                removeFromFavorites={() => removeFromFavorites(recipe)}
-                isFavorite={isFavorite}
+            <FavoriteToggle meal={recipe} />
+          </div>
+
+          <CategoriesLink
+            category={recipe.strCategory || ''}
+            country={recipe.strArea || ''}
+          />
+        </div>
+
+        <div className='flex flex-col-reverse sm:grid grid-cols-2 gap-10'>
+          <div className=''>
+            <p className='leading-relaxed text-xl text-justify mb-4'>
+              {recipe.strInstructions}
+            </p>
+          </div>
+
+          <div>
+            <div className='rounded-3xl overflow-hidden mb-10'>
+              <RecipeImage
+                imgLink={recipe.strMealThumb}
+                altText={recipe.strMeal}
+                imgPlaceholder={imgPlaceholder}
               />
             </div>
-
-            <CategoriesLink
-              category={recipe.strCategory || ''}
-              country={recipe.strArea || ''}
-            />
+            <Ingredients props={recipe} />
           </div>
+        </div>
 
-          <div className='flex flex-col-reverse sm:grid grid-cols-2 gap-10'>
-            <div className=''>
-              <p className='leading-relaxed text-xl text-justify mb-4'>
-                {recipe.strInstructions}
-              </p>
-            </div>
-
-            <div>
-              <div className='rounded-3xl overflow-hidden mb-10'>
-                <RecipeImage
-                  imgLink={recipe.strMealThumb}
-                  altText={recipe.strMeal}
-                  imgPlaceholder={imgPlaceholder}
-                />
-              </div>
-              <Ingredients props={recipe} />
-            </div>
-          </div>
-
-          {youtubeLink.length ? (
-            <YoutubeIframe address={youtubeLink.slice(32)} />
-          ) : null}
-        </Layout>
-      )}
+        {youtubeLink.length ? (
+          <YoutubeIframe address={youtubeLink.slice(32)} />
+        ) : null}
+      </Layout>
     </>
   );
 };
