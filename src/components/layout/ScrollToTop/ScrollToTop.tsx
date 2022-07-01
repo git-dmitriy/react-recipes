9d9c477aeea6c40047e59bd5b03c510207ec8d84
git-dmitriy <shalberkind@mail.ru>
2022-07-01 21:19:09 +0300

    Create test for ScrollToTop component

--- src/components/layout/ScrollToTop/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop/ScrollToTop.tsx
@@ -4,11 +4,6 @@ import { useLocation } from 'react-router';
 export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  /* 
-    Test scroll after render
-  
-  */
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
