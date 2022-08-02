e1f98edd4e42d64106e17693db8e95282d75e15d
git-dmitriy <shalberkind@mail.ru>
2022-08-02 23:17:16 +0300

    Update test snapshots

--- src/components/layout/Layout/Layout.test.tsx
+++ src/components/layout/Layout/Layout.test.tsx
@@ -2,6 +2,12 @@ import renderer from 'react-test-renderer';
 import { Layout } from 'components/layout/Layout';
 
 it('should render correctly', () => {
-  const tree = renderer.create(<Layout />).toJSON();
+  const tree = renderer
+    .create(
+      <Layout>
+        <div />
+      </Layout>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
