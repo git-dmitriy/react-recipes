369bc824c8a972dddf9d17e608d81d1afae24cba
git-dmitriy <dmitriy.shalberkin@gmail.com>
2024-01-11 23:39:37 +0300

    feat: add theme switcher

---
+++ src/components/Theme/Theme.tsx
@@ -0,0 +1,28 @@
+import {useEffect, useContext, useRef} from 'react';
+import {AppContext} from '@context/AppContext';
+
+export const Theme = () => {
+    const {state} = useContext(AppContext);
+    const firstRender = useRef(true);
+
+    useEffect(() => {
+        if (firstRender.current === true) {
+            document.body.classList.add(
+                'bg-yellow-200',
+                'selection:bg-yellow-400',
+                'selection:text-gray-800',
+                'dark:bg-gray-800',
+                'dark:selection:bg-red-400',
+                'dark:selection:text-gray-900'
+            );
+            firstRender.current = false;
+        }
+        if (state.theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [state]);
+
+    return null;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    content: [
+        "./index.html",
+        "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
