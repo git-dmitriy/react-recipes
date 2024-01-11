a70ad5136d1c72fcf6663109831b418fc52a8bc0
git-dmitriy <dmitriy.shalberkin@gmail.com>
2024-01-11 23:56:54 +0300

    feat: add favorites page

---
+++ src/components/FavoriteToggle/FavoriteToggle.tsx
@@ -0,0 +1,56 @@
+import {
+    BsFillBookmarkPlusFill,
+    BsFillBookmarkCheckFill,
+} from 'react-icons/bs';
+
+import {useState, useEffect, useContext} from 'react';
+import {AppContext} from '@context/AppContext';
+import {MealItemTypes} from '@/appTypes';
+
+type P = {
+    meal: MealItemTypes;
+    isDark?: boolean;
+};
+
+export const FavoriteToggle: React.FC<P> = ({meal, isDark = false}) => {
+    const [isFavorite, setIsFavorite] = useState<boolean | null>(null);
+    const {state, addToFavorites, removeFromFavorites} = useContext(AppContext);
+
+    useEffect(
+        () =>
+            setIsFavorite(
+                !!state.favorites.find((item) => item.idMeal === meal.idMeal)
+            ),
+        [state.favorites]
+    );
+
+    const addToFavorite = () => {
+        addToFavorites(meal);
+    };
+
+    const removeFromFavorite = () => {
+        removeFromFavorites(meal);
+    };
+
+    return (
+        <div className='mx-2 mt-2'>
+            {isFavorite ? (
+                <button
+                    className='text-2xl text-red-500 transition duration-200 uppercase'
+                    onClick={removeFromFavorite}
+                >
+                    <BsFillBookmarkCheckFill className='fill-current'/>
+                </button>
+            ) : (
+                <button
+                    className={`text-2xl text-gray-900 ${
+                        isDark ? '' : 'dark:text-orange-200'
+                    } hover:text-red-500 transition duration-200 uppercase`}
+                    onClick={addToFavorite}
+                >
+                    <BsFillBookmarkPlusFill className='fill-current'/>
+                </button>
+            )}
+        </div>
+    );
+};
---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { AppContext } from '@context/AppContext';
+import { MealsList } from '@components/MealsList';
+import { Layout } from '@components/layout/Layout';
+
+export const FavoritesPage: React.FC = () => {
+    const { state } = useContext(AppContext);
+
+    if (state.favorites.length === 0) {
+        return (
+            <Layout>
+                <div className='flex justify-center'>
+                    <h2 className='mx-auto font-bold'>
+                        You haven&apos;t added any recipes yet.
+                    </h2>
+                </div>
+            </Layout>
+        );
+    }
+    return (
+        <Layout>
+            <MealsList meals={state.favorites} />
+        </Layout>
+    );
+};
