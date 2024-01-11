14abd17427847d264554e2abf3766892a0b5ee3f
git-dmitriy <dmitriy.shalberkin@gmail.com>
2024-01-11 23:51:13 +0300

    feat: add meals-list component

---
+++ src/components/MealsList/MealsList.tsx
@@ -0,0 +1,38 @@
+import {Meal} from '@components/Meal';
+import {MealItemTypes} from '@/appTypes';
+// import { motion } from 'framer-motion';
+
+type P = {
+    meals: MealItemTypes[];
+};
+
+export const MealsList: React.FC<P> = ({meals}) => {
+    const variants = {
+        visible: (item: number) => ({
+            opacity: 1,
+            transition: {
+                delay: item * 0.03,
+            },
+        }),
+        hidden: {opacity: 0},
+    };
+
+    return (
+        <ul className='mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 justify-items-center xl:max-w-6xl max-w-4xl'>
+            {meals.map((meal, idx) => (
+                // <motion.li
+                //     key={meal.idMeal}
+                //     className='flex'
+                //     variants={variants}
+                //     initial='hidden'
+                //     animate='visible'
+                //     custom={idx}
+                // >
+                <li key={meal.idMeal} className='flex'>
+                    <Meal {...meal} />
+                </li>
+                // </motion.li>
+            ))}
+        </ul>
+    );
+};
