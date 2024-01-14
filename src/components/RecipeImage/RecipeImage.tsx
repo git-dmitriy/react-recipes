ccd035072d000031f4de0f9828c5753ad1c66c24
git-dmitriy <dmitriy.shalberkin@gmail.com>
2024-01-14 23:31:16 +0300

    feat: add recipe page

---
+++ src/components/RecipeImage/RecipeImage.tsx
@@ -0,0 +1,25 @@
+import {RecipeImageTypes} from '@/appTypes';
+
+export const RecipeImage: React.FC<RecipeImageTypes> = ({
+                                                            imgLink,
+                                                            altText,
+                                                            imgPlaceholder,
+                                                        }) => {
+    if (!imgLink) {
+        return (
+            <img
+                className='object-cover object-center w-full'
+                src={imgPlaceholder}
+                alt={altText}
+            />
+        );
+    }
+
+    return (
+        <img
+            className='object-cover object-center w-full'
+            src={imgLink}
+            alt={altText}
+        />
+    );
+};
---
+++ src/components/YoutubeIframe/YoutubeIframe.tsx
@@ -0,0 +1,19 @@
+type P = {
+    address: string;
+};
+
+export const YoutubeIframe: React.FC<P> = ({address}) => {
+    return (
+        <div className='relative h-0 mt-8' style={{paddingBottom: '56.25%'}}>
+            <iframe
+                className='absolute top-0 left-0 w-full h-full'
+                title='Video instruction'
+                width='100%'
+                height='500px'
+                src={`https://www.youtube.com/embed/${address}`}
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
+            ></iframe>
+        </div>
+    );
+};
