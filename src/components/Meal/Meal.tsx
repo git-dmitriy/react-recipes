6e553f85abeb6bc1abeffebd7eeb852e36c01dc0
git-dmitriy <dmitriy.shalberkin@gmail.com>
2025-04-07 23:16:33 +0300

    fix: fix meal card click

--- src/components/Meal/Meal.tsx
+++ src/components/Meal/Meal.tsx
@@ -6,27 +6,28 @@ export const Meal: React.FC<MealItemTypes> = (props) => {
     const {idMeal, strMeal, strMealThumb} = props;
 
     return (
-        <Link
-            to={`/meal/${idMeal}`}
+        <article
             className='block min-h-300 hover:opacity-90 focus-visible:opacity-90 transition-opacity relative w-full z-0 bg-white dark:bg-gray-100 rounded-3xl overflow-hidden border border-gray-200 dark:border-gray-900 dark:text-gray-900'
         >
-            <img
-                className='min-w-full aspect-ratio-4/3'
-                width='355'
-                height='437'
-                src={strMealThumb}
-                alt={strMeal}
-            />
-            <div className='text-center my-2 mx-3 min-h-full'>
-                <h4 className='text-2xl font-semibold line-clamp-1'>
-                    {strMeal}
-                </h4>
-            </div>
+            <Link to={`/meal/${idMeal}`}>
+                <img
+                    className='min-w-full aspect-ratio-4/3'
+                    width='355'
+                    height='437'
+                    src={strMealThumb}
+                    alt={strMeal}
+                />
+                <div className='text-center my-2 mx-3 min-h-full'>
+                    <h4 className='text-2xl font-semibold line-clamp-1'>
+                        {strMeal}
+                    </h4>
+                </div>
+            </Link>
 
             <div
                 className='absolute top-4 right-4 p-1 bg-white/50 backdrop-opacity-50 backdrop-filter backdrop-blur-2xl rounded-2xl'>
                 <FavoriteToggle isDark={true} meal={props}/>
             </div>
-        </Link>
+        </article>
     );
 };
