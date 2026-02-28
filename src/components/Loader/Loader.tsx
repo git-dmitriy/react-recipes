60034b7fb433c197bc65226ee60697fe298882f8
git-dmitriy <shalberkind@mail.ru>
2026-02-28 18:34:18 +0300

    refactor: update loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,20 +1,17 @@
-import {motion} from 'motion/react';
-
 export const Loader = () => {
-    const circle = 'h-5 w-5 bg-yellow-400 rounded-full';
+    const circle = 'h-5 w-5 bg-yellow-400 rounded-full loader-dot';
 
     return (
-        <motion.div
-            transition={{duration: 0.3}}
-            role='alert'
-            aria-busy='true'
-            className={`fixed inset-0 flex justify-center items-center my-20`}
+        <div
+            role="alert"
+            aria-busy="true"
+            className="fixed inset-0 flex justify-center items-center my-20"
         >
-            <div className='flex'>
-                <div className={`${circle} mr-1 animate-bounce`}></div>
-                <div className={`${circle} mr-1 animate-bounce200`}></div>
-                <div className={`${circle} animate-bounce400`}></div>
+            <div className="flex gap-1">
+                <div className={`${circle} animate-bounce`} />
+                <div className={`${circle} animate-bounce200`} />
+                <div className={`${circle} animate-bounce400`} />
             </div>
-        </motion.div>
+        </div>
     );
 };
