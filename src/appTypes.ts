c5ed514eb4e1f4b0472334765e77bc38f0e10f24
git-dmitriy <shalberkind@mail.ru>
2026-03-01 00:05:42 +0300

    chore: remove unused types

--- src/appTypes.ts
+++ src/appTypes.ts
@@ -1,10 +1,3 @@
-import {
-    ADD_TO_FAVORITE,
-    REMOVE_FROM_FAVORITE,
-    SWITCH_THEME,
-    SET_IS_LOADING,
-} from '@context/contextTypes';
-
 export type CategoryItemTypes = {
     idCategory: string;
     strCategory: string;
@@ -27,33 +20,8 @@ export type MealItemTypes = {
     strYoutube?: string;
 };
 
-export type RecipeItemTypes = {
-    idMeal: string;
-    strMeal: string;
-    strMealThumb: string;
-};
 
 export type RecipeImageTypes = {
     imgLink?: string;
     altText: string;
-};
-
-export type StateTypes = {
-    favorites: MealItemTypes[] | [];
-    theme: string | 'light' | 'dark';
-    isLoading: boolean;
-};
-
-export type ActionTypes =
-    | { type: typeof ADD_TO_FAVORITE; payload: MealItemTypes }
-    | { type: typeof REMOVE_FROM_FAVORITE; payload: MealItemTypes }
-    | { type: typeof SWITCH_THEME; payload: 'light' | 'dark' }
-    | { type: typeof SET_IS_LOADING; payload: boolean };
-
-export type ContextTypes = {
-    state: StateTypes;
-    addToFavorites: (item: MealItemTypes) => void;
-    removeFromFavorites: (item: MealItemTypes) => void;
-    switchTheme: (mode: 'light' | 'dark') => void;
-    setIsLoading: (status: boolean) => void;
 };
